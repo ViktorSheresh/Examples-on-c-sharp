be601eb618f304bc2f3c053d811f80c369b889ce
Віктор Шереш <sheregi1000@gmail.com>
2019-08-29 20:50:10 +0300

    Recursion

---
+++ Recursion/Recursion/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion
+{
+    class Program
+    {
+        static void Recursion (int value)
+        {
+            value--;
+            Console.WriteLine("Value {0}", value);
+
+            if (value != 0)
+                Recursion(value);
+            Console.WriteLine("Value {0}", value);
+        }
+
+        static void Main(string[] args)
+        {
+            Recursion(10);
+        }
+    }
+}
