1e161e686094e7a7e3c55d05735dea1e99734dbf
Віктор Шереш <sheregi1000@gmail.com>
2019-09-01 17:12:23 +0300

    Power

---
+++ Logic/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Logic
+{
+    class Program
+    {
+        static ulong Power (uint x, uint n)
+        {
+            ulong res = 1;
+            for (int i = 0; i <= n; i++)
+			{
+                if (n == 0)
+	            {           
+                    return (ulong)Math.Pow(x,0);
+	            }
+                res = (ulong)Math.Pow(x,n);
+			}
+            return res;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Power(2,10));
+        }
+    }
+
+
+}
