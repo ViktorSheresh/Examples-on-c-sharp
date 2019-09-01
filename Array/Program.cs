f35bd35e9ed266683c5bcd3391c2181dd7366d9a
Віктор Шереш <sheregi1000@gmail.com>
2019-09-01 20:48:05 +0300

    Array

---
+++ Array/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of array elements");
+
+            int elementsNumber = Convert.ToInt32(Console.ReadLine());
+
+            int[] array = new int[elementsNumber];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = i * 2;
+
+                Console.WriteLine("Arrays elements {0}", array[i]);
+            }
+        }
+    }
+}
