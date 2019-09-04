09c41f7baff8a344f0cf05ab23936f0e6b98b570
Віктор Шереш <sheregi1000@gmail.com>
2019-09-04 22:02:51 +0300

    Arrays

--- Arrays_001/Program.cs
+++ Arrays_001/Program.cs
@@ -10,11 +10,32 @@ namespace Arrays_001
     {
         static void Main(string[] args)
         {
-            byte[] byte_array = new byte[4] { 2, 4, 7, 15 };
-            Console.WriteLine(byte_array[0]);
-            Console.WriteLine(byte_array[1]);
-            Console.WriteLine(byte_array[2]);
-            Console.WriteLine(byte_array[3]);
+            //byte[] byte_array = new byte[4] { 2, 4, 7, 15 };
+            //Console.WriteLine(byte_array[0]);
+            //Console.WriteLine(byte_array[1]);
+            //Console.WriteLine(byte_array[2]);
+            //Console.WriteLine(byte_array[3]);
+
+            int[,] array = new int[4, 4];
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    array[i, j] = i * j;
+                }
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.Write("{0} ", array[i, j]);
+                }
+                Console.WriteLine();
+
+            }
+
         }
     }
 }
