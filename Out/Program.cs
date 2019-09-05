2e2ad5b14cca3cb6d358fe048cb7b7a1d4dae8ef
Віктор Шереш <sheregi1000@gmail.com>
2019-09-05 22:22:32 +0300

    Out

---
+++ Out/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Out
+{
+    class Program
+    {
+        static void Function(out int value, out int value1)
+        {
+            value = 100;
+            value1 = 200;
+        }
+
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            //Console.WriteLine(a);
+            Function(out a, out b);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
