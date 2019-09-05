353646026739dcf4ef3f81b57148d929a2b548c7
Віктор Шереш <sheregi1000@gmail.com>
2019-09-05 22:08:47 +0300

    Red

---
+++ Ref/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ref
+{
+    class Program
+    {
+        static int Reference (ref int value)
+        {
+            value = 999;
+            return value;
+        }
+
+        static void Main(string[] args)
+        {
+            int b = 100;
+            Console.WriteLine("b before ref modificator {0}", b);
+            Reference(ref b);
+            Console.WriteLine("b after ref modificator {0}", b);
+
+        }
+    }
+}
