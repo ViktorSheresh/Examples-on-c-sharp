38eeefcd4c86ebd30206f7d29d9e3a86cad47567
Віктор Шереш <sheregi1000@gmail.com>
2019-09-05 20:00:40 +0300

    Overflow

---
+++ Overflow/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 500;
+            byte b;
+            checked
+            {
+            b = (byte)a;
+            }
+            Console.WriteLine(b);
+        }
+    }
+}
