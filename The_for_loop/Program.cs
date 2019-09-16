f7435605e6b6f9b729c4f90f343166210cbe2f2a
Віктор Шереш <sheregi1000@gmail.com>
2019-09-16 17:53:14 +0300

    The infinite loop

---
+++ The_for_loop/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_for_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (; ; )
+            {
+                Console.Write("Enter the parol: ");
+                string parol = Console.ReadLine();
+                if (parol != "1234")
+                {
+                    Console.WriteLine("Enter the parol once again");                    
+                }
+                else
+                {
+                    Console.WriteLine("Congratulation, you are entered the right parol");
+                    break;
+                }
+
+            }
+        }
+    }
+}
